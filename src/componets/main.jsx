55f4d6ca85a04e50862720207cc782bfe898b735
Aaron Trelstad <108637601+AaronTrelstad@users.noreply.github.com>
2023-04-07 02:19:16 +0000

    1st edit

---
+++ src/componets/main.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import './main.css'
+
+const Background = () => {
+    return (
+        <>
+            <div>
+                <h1>Project 1</h1>
+                <h2>From Scratch</h2>
+            </div>
+
+            <div className='box1'>
+                <article className='minibox'>
+                    <h3>
+                        Website Goals
+                    </h3>
+                    <p>
+                        Learn basic features but also learn how to make cool animations and integrate JavaScript backend.
+                    </p>
+                </article>
+            </div>
+        </>
+        
+    )
+}
+
+export default Background
